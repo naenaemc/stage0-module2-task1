c2d46148e29ab071922ed802fc148f041211fe20
naenaemc <user@DESKTOP-DMN5P17>
2026-07-11 22:47:50 +0300

    2

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,6 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-    	System.out.println("Hello, world!");// Write your code here!
+    	System.out.println("Hello, world!");
     }
 }
